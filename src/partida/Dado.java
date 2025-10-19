ca9d24144eba94d43d65f3bef9725d7b76422623
Daniel <daniel.pazos.castro@rai.usc.es>
2025-10-19 11:42:02 +0200

    Merge remote-tracking branch 'origin/main'

--- src/partida/Dado.java
+++ src/partida/Dado.java
@@ -25,27 +25,6 @@ public class Dado {
         return this.valor;
     }
 
-    // Método para forzar el dado a un valor específico
-    public void forzarDado(int valor) {
-        if (valor >= 1 && valor <= 6) {
-            this.forzado = true;
-            this.valorForzado = valor;
-            System.out.println("Dado forzado a: " + valor);
-        } else {
-            System.out.println("Error: El valor del dado debe estar entre 1 y 6");
-        }
-    }
-
-    // Método para desactivar el forzado
-    public void desactivarForzado() {
-        this.forzado = false;
-        System.out.println("Forzado de dados desactivado");
-    }
-
-    // Método para verificar si el dado está forzado
-    public boolean isForzado() {
-        return forzado;
-    }
 
     //Getters y setters:
     public int getValor() {
