cf987ea49a42c06c23bf463a5f022d6c863805b0
Daniel <daniel.pazos.castro@rai.usc.es>
2025-11-27 10:58:13 +0100

    Casilla y Grupo

--- src/monopoly/casilla/accion/Suerte.java
+++ src/monopoly/casilla/accion/Suerte.java
@@ -1,7 +1,35 @@
 package monopoly.casilla.accion;
 
 import monopoly.casilla.Accion;
+import partida.Jugador;
+import partida.Avatar;
 
 public class Suerte extends Accion {
 
+    // Constructor
+    public Suerte(String nombre, int posicion, Jugador duenho) {
+        super(nombre, posicion, duenho, "Suerte");
+    }
+
+    // MÉTODO de evaluación de casilla - Específico para Suerte
+    @Override
+    public boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada) {
+        if (actual.getAvatar().getLugar() == this) {
+            System.out.println("¡Has caído en Suerte! Se elegirá una carta de suerte.");
+            // La ejecución de la carta se manejará en el juego principal
+            return true;
+        }
+        return false;
+    }
+
+    // MÉTODO de información - Específico para Suerte
+    @Override
+    public void infoCasilla() {
+        System.out.println("{");
+        System.out.println("\tTipo: Acción");
+        System.out.println("\tSubtipo: Suerte");
+        System.out.println("\tNombre: " + this.getNombre());
+        System.out.println("\tAcción: Robar carta de Suerte");
+        System.out.println("}");
+    }
 }
