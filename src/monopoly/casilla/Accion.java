35d55658f685705262e3d9ea314db32c527f1aa3
Daniel <daniel.pazos.castro@rai.usc.es>
2025-11-27 10:28:00 +0100

    Merge remote-tracking branch 'origin/main'

--- src/monopoly/casilla/Accion.java
+++ src/monopoly/casilla/Accion.java
@@ -1,4 +1,49 @@
 package monopoly.casilla;
 
-public class Accion extends Casilla{
+import partida.Jugador;
+import partida.Avatar;
+
+public abstract class Accion extends Casilla {
+
+    // Constructor
+    public Accion(String nombre, int posicion, Jugador duenho) {
+        super(nombre, posicion, duenho);
+    }
+
+    // MÉTODOS REQUERIDOS por el PDF - IMPLEMENTACIÓN
+    @Override
+    public boolean estaAvatar(Avatar avatar) {
+        return this.avatares.contains(avatar);
+    }
+
+    @Override
+    public int frecuenciaVisita() {
+        return this.contadorVisitas;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Accion{nombre='%s', posicion=%d, tipo=%s}",
+                nombre, posicion, this.getClass().getSimpleName());
+    }
+
+    // MÉTODO COMÚN para acciones - será sobrescrito
+    @Override
+    public abstract boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada);
+
+    // MÉTODO de información - será sobrescrito
+    @Override
+    public abstract void infoCasilla();
+
+    // Las casillas de acción no tienen valor monetario
+    @Override
+    public float getValor() {
+        return 0;
+    }
+
+    // Las casillas de acción no son comprables
+    @Override
+    public boolean esTipoComprable() {
+        return false;
+    }
 }
--- src/monopoly/casilla/Casilla.java
+++ src/monopoly/casilla/Casilla.java
@@ -105,3 +105,4 @@ public abstract class Casilla {
     }
 }
 
+
