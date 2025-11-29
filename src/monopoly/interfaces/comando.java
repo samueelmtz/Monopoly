d76ad0685bc83c01f55a034363f42e61663d424c
samueelmtz <samueel.mtz17@gmail.com>
2025-11-29 12:27:47 +0100

    Comando

--- src/monopoly/interfaces/comando.java
+++ src/monopoly/interfaces/comando.java
@@ -1,4 +1,24 @@
 package monopoly.interfaces;
 
-public interface comando {
+import partida.Jugador;
+import monopoly.casilla.Casilla;
+
+public interface comando{
+    void crearJugador(String nombre, String tipoAvatar);
+    void turnoJugador();
+    void descJugador(String[] partes);
+    void descCasilla(String nombre);
+    void lanzarDados(String valoresForzados);
+    void comprar(String nombre);
+    void salirCarcel();
+    void listarVenta();
+    void listarJugadores();
+    void listarEdificios(String colorGrupo);
+    void acabarTurno();
+    void mostrarEstadisticas(String nombreJugador);
+    void mostrarEstadisticasJuego();
+    void edificar(String tipoEdificio);
+    void venderEdificios(String tipoVenta, String nombreCasilla, int cantidadSolicitada);
+    void hipotecarPropiedad(String nombreCasilla);
+    void deshipotecarPropiedad(String nombreCasilla);
 }
