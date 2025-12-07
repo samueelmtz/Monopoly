c29a94102feb3da672b0c384c6a453f902e5f24f
Daniel <daniel.pazos.castro@rai.usc.es>
2025-12-07 11:10:47 +0100

    Merge remote-tracking branch 'origin/main'

--- src/excepciones/ExcepcionComandoNoReconocido.java
+++ src/excepciones/ExcepcionComandoNoReconocido.java
@@ -3,14 +3,9 @@ package excepciones;
 // Nivel 3 jerarquia - Comando no reconocido por el sistema
 public class ExcepcionComandoNoReconocido extends ExcepcionAccion {
 
-    private final String comando;
-
-    public ExcepcionComandoNoReconocido(String comando) {
-        super("Comando no reconocido: '" + comando + "'");
-        this.comando = comando;
-    }
-
-    public String getComando() {
-        return comando;
+    // Nuevo constructor con formato sugerido
+    public ExcepcionComandoNoReconocido(String formatoCorrecto, String comandoIncorrecto) {
+        super("Comando incorrecto: '" + comandoIncorrecto + "'. " +
+                "Formato correcto: " + formatoCorrecto + ".");
     }
 }
